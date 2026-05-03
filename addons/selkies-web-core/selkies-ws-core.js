9255788340e01664ab920f3f7dc6db7185e96330
thelamer <ryankuba@gmail.com>
2026-05-03 14:36:16 -0400

    bad var

--- addons/selkies-web-core/selkies-ws-core.js
+++ addons/selkies-web-core/selkies-ws-core.js
@@ -3500,11 +3500,9 @@ document.addEventListener('DOMContentLoaded', () => {
                    applyManualCanvasStyle(manual_width, manual_height, true);
                  }
 
-                 if (sharedClientState === 'ready' && dimensionsChanged && identifiedEncoderModeForShared === 'h264_full_frame') {
-                   console.log(`Shared mode: Triggering main decoder re-init for new resolution.`);
+                 if (sharedClientState === 'ready' && dimensionsChanged) {
+                   console.log(`Shared mode: Triggering main decoder re-init and clearing canvas for new resolution.`);
                    triggerInitializeDecoder();
-                 } else if (sharedClientState === 'ready' && dimensionsChanged) {
-                   console.log(`Shared mode: Clearing canvas due to resolution change.`);
                    if (canvasContext && canvas.width > 0 && canvas.height > 0) {
                      canvasContext.setTransform(1, 0, 0, 1, 0, 0);
                      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
